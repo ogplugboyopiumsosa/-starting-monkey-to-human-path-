bc4c65d52a3ccce3b34db00d0d6df77b91dff2d6
opiumsosa <opiumsosa@gmail.com>
2024-12-21 09:29:02 +0300

    Added entry point

---
+++ 323/Kocherov/wdad/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starting_monkey_to_human_path._323.Kocherov.wdad
+{
+    internal class Application
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("I’m Егор Кочеров, and I’m not a monkey");
+        }
+    }
+}
